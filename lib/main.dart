6c540b8d9b3c7600818701a078f5879397e2ad41
Waseem Iqbal <shahzaibiqbal851@gmail.com>
2025-02-12 12:06:31 +0500

    module1

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Azra Naheed Medical College',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Simulate a delay for the splash screen
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('assets/logo.png'), // Add your logo image here
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Azra Naheed Medical College')),
+      body: Center(
+        child: Text('Welcome to the Azra Naheed Medical College App!'),
+      ),
+    );
+  }
+}
